d41a195f997cd6f53ba619ea9a6b4db24d1c58c1
Cole Landers <jclanders@pm.me>
2026-04-04 00:10:09 -0500

    spaghetti -> lasagna B)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,41 @@
-use std::{i32::MAX, io::{self, Read}, process::exit};
+use std::{io::{self}, process::exit};
 
 use num_complex::Complex;
 use soapysdr::RxStream;
+
+static DF_LENGTH: usize = 10; // 5 bits, 10 samples @ 2_000_000 sample rate
+static PREAMBLE_LENGTH: usize = 16;
+const PREAMBLE_PATTERN: [bool;PREAMBLE_LENGTH]= [
+    true, 
+    false,
+    true,
+    false,
+    false, 
+    false,
+    false, 
+    true,
+    false, 
+    true,
+    false, 
+    false,
+    false, 
+    false,
+    false, 
+    false,
+];
+
+struct SkywatchState {
+    samples_buffer: Vec<Complex<f32>>,
+    hit_threshold: f32,
+}
+
+fn init_skywatch() -> Result<SkywatchState, ()> {
+    let state= SkywatchState {
+        samples_buffer: vec![Complex::new(0.0f32, 0.0f32); 1_024_000],
+        hit_threshold: 0.01f32
+    };
+    return Ok(state);
+}
 
 fn init_soapysdr() -> Result<RxStream<Complex<f32>>, soapysdr::Error> {
     let mut devices = soapysdr::enumerate("").expect("Devices could not be retrieved.");
@@ -34,78 +68,74 @@ fn init_soapysdr() -> Result<RxStream<Complex<f32>>, soapysdr::Error> {
     return Ok(rx_stream);
 }
 
-fn main() {
-    let mut rx_stream = init_soapysdr().expect("Could not initialize SoapySDR");
-    let mut buffer = vec![Complex::new(0.0f32, 0.0f32); 1_024_000];
-    let threshold = 0.01f32;
-    let preamble_pattern = [
-        true, 
-        false,
-        true, 
-        false,
-        false, 
-        false,
-        false, 
-        true,
-        false, 
-        true,
-        false, 
-        false,
-        false, 
-        false,
-        false, 
-        false,
-    ];
-    loop {
-        let samples_read = rx_stream.read(&mut [buffer.as_mut_slice()], 1_000_000).expect("Rx Stream could not successfully read");
-        let magnitudes: Vec<f32> = buffer[..samples_read].iter().map(|s|s.norm()).collect();
-        // println!("{} - {}", samples_read, magnitudes.len());
-        // loop over buffer and search for preamble
-        let mut i = 0;
-        let mut overflow = false;
-        while i < (samples_read - preamble_pattern.len()) {
-            let mut preamble_detected = false;
-            if magnitudes[i] > threshold {
-                for j in 0..preamble_pattern.len() {
-                    i = i + j;
-                    if i >= samples_read {
-                        overflow = true;
-                        preamble_detected = false;
-                        break;
-                    }
-                    if ((magnitudes[i] > threshold).ne(&preamble_pattern[j])) {
-                        preamble_detected = false;
-                        break;
-                    } else {
-                        preamble_detected = true;
-                    }
-                }
-                if preamble_detected {
-                    if i + 10 > samples_read { // 10 samples = 5microseconds = 5bits
-                        break;
-                    } else {
-                        let mut df = 0u8;
-                        let df_buffer = &magnitudes[i..i+10];
-                        for bit in 0..5 {
-                            let first = df_buffer[bit * 2];
-                            let second= df_buffer[(bit * 2) + 1];
-                            if first > second {
-                                df |= 1 << (4 - bit);
-                            }
-                        }
-                        if df == 17 {
-                            // Hit ADS-B
-                        }
-                    }
-                }
-                if overflow {
-                    println!("Overflowed");
-                    break;
-                }
-            } else {
-                i = i + 1;
-            }
+fn process_samples(state: &SkywatchState)
+{
+    let samples_read = state.samples_buffer.len();
+    
+    // Convert samples from raw I/Q data to scalar values; expensive, so do once here each loop
+    // I believe norm_sqrt is supposed to be more efficient if ever needed
+    let magnitudes: Vec<f32> = state.samples_buffer[..samples_read].iter().map(|s|s.norm()).collect();
+    
+    let mut i = 0;
+    while i < samples_read {
+        // Preamble
+        if (i + PREAMBLE_LENGTH) >= samples_read {
+            break;
         }
+        let preamble: [f32; PREAMBLE_LENGTH] = magnitudes[i..i + PREAMBLE_LENGTH]
+            .try_into()
+            .expect("slice length is always PREAMBLE_LENGTH");
+        let preamble_detected = check_preamble(state, preamble);
+        if preamble_detected {
+            i = i + PREAMBLE_LENGTH;
+            // DF
+            if (i + DF_LENGTH) >= samples_read {
+                break;
+            }
+            let df_buffer: [f32; DF_LENGTH] = magnitudes[i..i + DF_LENGTH]
+                .try_into()
+                .expect("slice length is always DF_LENGTH");
+            let df = extract_df(df_buffer);
+            println!("Preamble Hit: DF-{}", df);
+            i = i + DF_LENGTH;
+            // TC
+        } else {
+            i = i + 1;
+        }
+    }
+}
+
+fn check_preamble(state: &SkywatchState, magnitude_buffer: [f32; PREAMBLE_LENGTH]) -> bool {
+    let mut result = true;
+    for i in 0..PREAMBLE_LENGTH {
+        if (magnitude_buffer[i] > state.hit_threshold).ne(&PREAMBLE_PATTERN[i]) {
+            result = false;
+            break;
+        }
+    }
+
+    return result;
+}
+
+fn extract_df(df_buffer: [f32; DF_LENGTH]) -> u8 {
+    let mut df = 0u8;
+    for bit in 0..5 {
+        let first = df_buffer[bit * 2];
+        let second= df_buffer[(bit * 2) + 1];
+        if first > second {
+            df |= 1 << (4 - bit);
+        }
+    }
+
+    return df;
+}
+
+fn main() {
+    let mut state = init_skywatch().unwrap();
+    let mut rx_stream = init_soapysdr().expect("Could not initialize SoapySDR");
+    loop {
+        let _ = rx_stream.read(&mut [state.samples_buffer.as_mut_slice()], 1_000_000).expect("Rx Stream could not successfully read");
+        process_samples(&state);
     }
     
     rx_stream.deactivate(None).expect("Rx Stream could not successfully deactivate");
