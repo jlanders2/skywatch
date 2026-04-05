c20c27975b03aca23a76edf0fa35bce64c5b06f0
Cole Landers <jclanders@pm.me>
2026-04-04 21:31:43 -0500

    set ads-b defaults in main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use sdr_core::SdrDirection;
+
 fn main() {
     let sdr = skywatch_runtime::sdr_factory("soapysdr");
-    let device = sdr.create_device().unwrap();
+    let mut device = sdr.create_device().unwrap();
+    // Assume all these let _ are an anti-pattern
+    let _ = device.set_channel(0);
+    let _ = device.set_direction(SdrDirection::Receive);
+    let _ = device.set_frequency(1_090_000_000.0);
+    let _ = device.set_sample_rate(2_000_000.0);
+    let _ = device.set_gain(40.0);
+
     let mut stream = device.get_stream().unwrap();
     loop {
         let samples = stream.read().unwrap();
